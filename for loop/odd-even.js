96727f6cdf2c05327f8a55649ba010a65cf32686
fahimmuntacher <muntacherfahim234@gmail.com>
2025-07-29 22:39:24 +0600

    problem 2 fully done

---
+++ for loop/odd-even.js
@@ -0,0 +1,13 @@
+// odd number finding
+for (let i = 60; i <= 100; i++){
+    if(i % 2 !== 0){
+        // console.log("The even number is:", i)
+    }
+}
+
+// even number finding
+for(let i = 78; i <= 98; i++){
+    if(i % 2 == 0){
+        console.log("The even num is:", i);
+    }
+}
--- for loop/odd.js
+++
@@ -1,5 +0,0 @@
-for (let i = 60; i <= 100; i++){
-    if(i % 2 !== 0){
-        console.log("The even number is:", i)
-    }
-}
